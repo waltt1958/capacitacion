e2cf0d49656b0918d66540def6b15e1c4dd6ad86
waltt1958 <ovidiov1958@gmail.com>
2019-01-17 17:27:46 -0300

    continuo modif contra

--- modifContra.aspx.vb
+++ modifContra.aspx.vb
@@ -23,29 +23,38 @@ Partial Class modifContra
         Dim sqlModifica As String
         Dim sqlBuscar As String
         Dim da As OleDbDataAdapter
-        Dim rutaCon As String
-        Dim dt As DataTable
+        Dim dt As DataSet
+        Dim con As New OleDbConnection
 
-        sqlBuscar = " select * from usuarios where Id_usuario= ?"
-        rutaCon = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Server.MapPath("/bbdd/capacitacion.accdb")
+        sqlBuscar = " select * from usuarios where Id_usuario=" & legajo.Text
+        con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Server.MapPath("/bbdd/capacitacion.accdb")
 
-        da = New OleDbDataAdapter(sqlBuscar, rutaCon)
-        dt = New DataTable
-        da = Fill(dt)
+        ' Dim cmd As New OleDbCommand(sqlBuscar, con)
+        ' cmd.Parameters.Add(New OleDbParameter("Id_usuario", OleDbType.VarChar, 10))
+        ' cmd.Parameters("Id_usuario").Value = legajo.Text
+
+        da = New OleDbDataAdapter(sqlBuscar, con)
+        dt = New DataSet
+        da.Fill(dt, "usuarios")
+
+
+        Response.Write(dt.Tables("usuarios").Rows(0).Item("Id_usuario"))
+
+
 
         'Dim cmd As New OleDbCommand(sqlBuscar, con)
         'cmd.Parameters.Add(New OleDbParameter("Id_usuario", OleDbType.VarChar, 10))
         'cmd.Parameters("Id_usuario").Value = legajo.Text
         'odataReader = cmd.ExecuteReader()
 
-        If odataReader.Read() = False Then
-            Response.Write("<script>window.alert('Nunca se ha registrado con ese legajo. ');</script>" +
-               "<script>window.setTimeout(location.href='altaUsuario.aspx', 2000);</script>")
-        ElseIf odataReader("Id_usuario") = legajo.Text And odataReader("contrasena") = contraVieja.Text Then
+        'If odataReader.Read() = False Then
+        ' Response.Write("<script>window.alert('Nunca se ha registrado con ese legajo. ');</script>" +
+        '   "<script>window.setTimeout(location.href='altaUsuario.aspx', 2000);</script>")
+        'ElseIf odataReader("Id_usuario") = legajo.Text And odataReader("contrasena") = contraVieja.Text Then
 
 
 
-        End If
+        'End If
 
     End Sub
 End Class
